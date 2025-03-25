cd223472c0b536d6aaebde0b5c6216a034d478ac
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2025-03-25 18:22:52 +0300

    Feat : Add test endoint with ugur

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetTest()
+        {
+            Random rnd = new Random();
+            return Ok($"Uguru bugün {rnd.Next(1, 100)} kez abi dedi");
+        }
+
+    }
+}
